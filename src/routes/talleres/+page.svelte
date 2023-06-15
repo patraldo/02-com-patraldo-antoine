d8f8f57e1f7ee8deedbdcca1eb71f509b132858b
cheftech <cheftech@patraldo.com>
2023-06-14 21:56:36 -0600

    update css, layout, pages

--- src/routes/talleres/+page.svelte
+++ src/routes/talleres/+page.svelte
@@ -1,3 +1,76 @@
-<h1>Talleres</h1>
+<svelte:head>
+<title>Antoine Patraldo, Talleres</title>
+</svelte:head>
 
-<h3>Taller abierto para artistas y tecnologistas en animación. En nuestra casa, su casa, tenemos un restaurante con café, Café Chapopote de ¡Pinche Poutine! En las horas abiertas sean bienvenid@s a inspirarte de l@s mus@s de Santa Tere. Aprovéchense los sábados.  A partir de las 10 de la mañana, café Canadiense a su antojo con cualquier de los desayunos del mes.</h3>
+<h1 style="font-size:10vw;">Talleres</h1>
+
+<p style="font-size:3vw;">Taller abierto para artistas y tecnologistas en animación. En nuestra casa, su casa, tenemos un restaurante con café, Café Chapopote de ¡Pinche Poutine! En las horas abiertas sean bienvenid@s a inspirarte de l@s mus@s de Santa Tere. Aprovéchense los sábados.  A partir de las 10 de la mañana, café Canadiense a su antojo con cualquier de los desayunos del mes.</p>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Marmelad&family=Pacifico&family=Special+Elite&family=Tangerine:wght@400;700&family=Ultra&display=swap');
+
+h1 {
+font-family: 'Special Elite', cursive;
+font-size: 3.0rem;
+padding: 20px 20px 60px;
+max-width: 680px;
+margin: 0 auto;
+}
+
+
+p {
+font-family: 'Special Elite', sans-serif;
+font-size: 1.875em;
+                }
+
+
+* {
+	box-sizing: border-box;
+}
+
+.row {
+display: -ms-flexbox; /* IE10 */
+display: flex;
+	 -ms-flex-wrap: wrap; /* IE10 */
+	 flex-wrap: wrap;
+padding: 0 4px;
+}
+
+/* create four equal columns that sits next to each other */
+.column {
+	-ms-flex: 25%; /* IE10 */
+flex: 25%;
+      max-width: 25%;
+padding: 0 4px;
+}
+
+.column img {
+	margin-top: 8px;
+	vertical-align: middle;
+width: 100%;
+}
+
+/* Responsive layout - makes a two column-layout instead of four columns */
+@media screen and (max-width: 800px) {
+	.column {
+		-ms-flex: 50%;
+flex: 50%;
+      max-width: 50%;
+	}
+}
+
+/* Responsive layout - makes the two columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 600px) {
+	.column {
+		-ms-flex: 100%;
+flex: 100%;
+      max-width: 100%;
+	}
+}
+</style>
+
+
+<div class="row">
+<div class="column"><img src="https://storage.googleapis.com/patraldo/antoine-dibujos/BG-2HM-Landscape.gif" alt="animation of drawing against background by Antoinex " onclick="window.open(this.src, '_blank')"></div>
+<div class="column"><img src="https://storage.googleapis.com/patraldo/antoine-dibujos/AGR-WomanDancingAgainstBackground.gif" alt="animation of drawing against background projected on cieling" onclick="window.open(this.src, '_blank')"></div>
+</div>
