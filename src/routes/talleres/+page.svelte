fb6ed3a40d5bf4c274d37c736ffe6c82a1495ae9
cheftech <cheftech@patraldo.com>
2023-06-16 22:47:03 -0600

    increased font size for Taller text

--- src/routes/talleres/+page.svelte
+++ src/routes/talleres/+page.svelte
@@ -1,5 +1,5 @@
 <div class='taller'>
-<p style="font-size:3vw;">Taller abierto para artistas y tecnologistas en animación. En nuestra casa, su casa, tenemos un restaurante con café, Café Chapopote de ¡Pinche Poutine! También es nuestro espacio para crear arte.</p> 
+<p style="font-size:6vw;">Taller abierto para artistas y tecnologistas en animación. En nuestra casa, su casa, tenemos un restaurante con café, Café Chapopote de ¡Pinche Poutine! También es nuestro espacio para crear arte.</p> 
 
 <p style="font-size:3vw;">Los sábados, a partir de las 10 de la mañana, café Canadiense a su antojo con cualquier de los desayunos del mes. Sean bienvenid@s a inspirarte de l@s mus@s de Santa Tere.</p>
 </div>
