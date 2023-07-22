414328872e5ef8a0c0ca81264419f30db5fc2916
Chef <cheftech@patraldo.dev>
2023-07-22 13:46:02 -0600

    update text exhibiciones

--- src/routes/exhibiciones/+page.svelte
+++ src/routes/exhibiciones/+page.svelte
@@ -7,12 +7,6 @@
 </script>
 <!-- <p style="font-size:5vw;">{path}Exhibiciones</p> -->
 
-<div class='bienvenido'> 
-  <h1 style="font-size:8vw;">Bienvenid@ a mi galería en Santa Tere, Guadalajara:</h1>
-</div>
-<div class='musa'> 
-  <h1 style="font-size:7vw;">La Musa que Más Aplauda</h1>
-</div>
 <div class='desc'>
   <p style="font-size:5vw;">Estamos preparando la exhibición inaugural de "Tray Chic". Ponemos que será en Octubre de 2023 en el lugar del difunto restaurante ¡Pinche Poutine! Más detalles próximamente.</p>
 </div>
@@ -20,34 +14,9 @@
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Marmelad&family=Pacifico&family=Special+Elite&family=Tangerine:wght@400;700&family=Ultra&display=swap');
 
-.bienvenido {
-border: 10px dotted black;
- background: #827179;
- background-clip: padding-box;
-font-family: 'Special Elite';
-margin: 10px;
-padding: 10px;
-text-align: center;
-color: white;
-}
-
-.musa {
-border: 10px dotted black;
- background: #ddd392;
- background-clip: padding-box;
-font-family: 'Special Elite';
-margin: 10px;
-padding: 10px;
-text-align: center;
-color: black;
-}
-
-.musa a:hover {
-color: green;
-}
 .desc {
 border: 10px dotted black;
- background: #6c8ea9;
+ background: #C3ABB3;
  background-clip: padding-box;
 font-family: 'Special Elite';
 margin: 10px;
